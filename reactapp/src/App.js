79595a7b29892244b84fcca41815539a8fa73ead
huseynashurlu1 <huseyn.ashurlu@code.edu.az>
2022-05-11 17:32:25 +0400

    reactrouter

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,51 +1,20 @@
-import React, { Component } from 'react';
-import ClassComp from './components/ClassComp';
-import FormEvent from './components/FormEvent';
-import FuncComp from './components/FuncComp';
-import Users from './components/Users';
+import './App.css'
 
-class App extends Component {
+const App = () => {
 
-  // state = {
-  //   users: [
-  //     {id:1,name:"Yamil",job: "Trader", salary: 7000},
-  //     {id:2,name:"Maqsud",job: "Video Editor", salary: 3500},
-  //     {id:3,name:"Maksud",job: "Video Editor", salary: 3500},
-  //     {id:4,name:"Togrul",job: "Car Seller", salary: 500},
-  //     {id:5,name:"Vusal",job: "Mentor", salary: 150},
-  //     {id:6,name:"Vusal",job: "Mentor", salary: 150},
-  //   ],
+  const [serach,setSearch] = useSta
 
-  //   worker: ''
-  // }
-
-  // deleteUser = (id) => {
-  //   this.setState({
-  //     users: this.state.users.filter(x => id !==x.id)
-  //   })
-  // }
-
-  // FilterHandler = (e) => {
-  //   this.setState({
-  //     worker: e.target.value
-  //   })
-  // }
-
-  render() {
-    // const {users,worker} = this.state;
-    // const filterUsers = users.filter(user=>user.name.toLowerCase().includes(worker.toLowerCase()))
-
-    return (
-      <div className='d-flex'>
-        <ClassComp />
-        <FuncComp/>
-      </div>
-      // <div className='container mt-4'>
-      //   <input onChange={this.FilterHandler} type="text" placeholder='Search...' className='form-control w-50 mx-auto mb-4' />
-      //   <Users filteredUsers={filterUsers} deleteUser={this.deleteUser} users={this.state.users}/>
-      // </div>
-    )
+  const HandleChange = (e) => {
+    console.log(e.target.value)
   }
-}
 
+  return (
+    <div className="app">
+      <h4>Search a cryptocurrency</h4>
+      <form action="">
+        <input onChange={HandleChange} className='search-input' type="text" placeholder="Search..." />
+      </form>
+    </div>
+  )
+}
 export default App
